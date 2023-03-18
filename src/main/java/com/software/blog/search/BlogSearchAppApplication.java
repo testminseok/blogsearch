1efae9934157c7c21f74d3fb15338093033e7bdd
testMinseok <opso_@naver.com>
2023-03-18 17:04:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/software/blog/search/BlogSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.software.blog.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchAppApplication.class, args);
+    }
+
+}
