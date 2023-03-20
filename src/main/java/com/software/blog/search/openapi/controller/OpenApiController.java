9dfb21635dd16db485799f4ca2f94ee4fa5db838
testMinseok <opso_@naver.com>
2023-03-21 00:00:44 +0900

    블로그 검색 횟수 동시성 처리 추가

--- src/main/java/com/software/blog/search/openapi/controller/OpenApiController.java
+++ src/main/java/com/software/blog/search/openapi/controller/OpenApiController.java
@@ -1,5 +1,6 @@
 package com.software.blog.search.openapi.controller;
 
+import com.software.blog.search.openapi.response.OpenApiResponse;
 import com.software.blog.search.openapi.service.OpenApiService;
 import com.software.blog.search.word.entity.SearchWord;
 import com.software.blog.search.word.repository.SearchWordRepository;
@@ -17,16 +18,25 @@ public class OpenApiController {
     private final OpenApiService kakaoOpenApiService;
 
     private final SearchWordService searchWordService;
+    
     private final SearchWordRepository searchWordRepository;
 
     @GetMapping("/search")
-    public Object search(String query, String sort, int page, int size) {
+    public OpenApiResponse search(String query, String sort, int page, int size) {
 
         // 검색어 검색횟수 증가
         searchWordService.findByWord(() -> searchWordRepository.findByWord(query))
                 .ifPresentOrElse(
-                        searchWord -> searchWordRepository.save(searchWord.increaseSearchCount()),
-                        () -> searchWordRepository.save(new SearchWord(query)));
+                        searchWord -> {
+                            synchronized (searchWordRepository) {
+                                searchWordRepository.save(searchWord.increaseSearchCount());
+                            }
+                        },
+                        () -> {
+                            synchronized (searchWordRepository) {
+                                searchWordRepository.save(new SearchWord(query));
+                            }
+                        });
 
         return kakaoOpenApiService.search(query, sort, page, size);
     }
